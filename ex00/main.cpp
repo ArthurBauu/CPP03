0a577b837f5283b9e18225f90f5a5208e32b7f9b
Arthur baudouin <arthurbaudouin05@gmail.com>
2025-06-10 00:22:57 +0200

    ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,21 @@
+#include "claptrap.hpp"
+
+int main() {
+	ClapTrap a("Terminator");
+	a.attack("Target Dummy");
+	a.takeDamage(3);
+	a.beRepaired(5);
+	a.attack("Enemy");
+
+	ClapTrap b = a;
+	b.takeDamage(10);
+	b.beRepaired(3);
+
+	ClapTrap c;
+	c.attack("Another Target");
+	c.takeDamage(2);
+	c.beRepaired(1);
+
+
+	return 0;
+}
